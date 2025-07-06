22d7ff5a3df838c71c95aacebd52ab171e15e02b
Ludgeroxd <ludgero834@gmail.com>
2025-07-06 15:36:59 -0300

    Initial commit

---
+++ src/test/java/com/rede/cadastro_usuario/CadastroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rede.cadastro_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
